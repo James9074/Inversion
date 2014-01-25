7e324d68386b9e5dd50fb74c9153ed378b05198a
Brandon Gibson <nudemanonbike@gmail.com>
2014-01-25 10:54:31 -0500

    UI changes

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class UI : MonoBehaviour {
 //IGNORE THIS FOR NOW! I SWEAR I WILL USE A BEARABLE (hehe bear) SYSTEM SOON!
 	// Use this for initialization
-	void Start () {
-		Resources.Load<Sprite>("Assests/sprites/inventory.png");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Resources.Load<Sprite>("Assests/sprites/inventory1.png");
-	}
+
+	public int selGridInt = 0;
+	public string[] selStrings = new string[] {"Empty", "Windbreaker", "Umbrella", "Sunglasses", "Gasmask", "Diet Soda", "Armor", "Flashlight", "Hard Hat", "Heels", "Spacesuit"};
+
+	void OnGUI () {
+		
+		selGridInt = GUILayout.SelectionGrid(new Rect(Screen.width/2 - 450, Screen.height/8, 900, 64), selGridInt, selStrings, 9);
+
+		}
 }
+ 
