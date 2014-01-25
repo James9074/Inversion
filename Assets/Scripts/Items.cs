fb4dc7b959fb326ac0b42f7e6d9d977c05855265
Eric King <emking92@gmail.com>
2014-01-25 00:42:04 -0500

    Added Items enum & temp reference to current item

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Items 
+{
+    Sunglasses,
+    Heels,
+    Umbrella,
+    DietSoda
+}
+
+public class Temp
+{
+    public static Items CurrentItem;
+}
