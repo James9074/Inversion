c7a4a122b3b43432bda9b797d09c39f650a5ff41
James Thomas <james9074@gmail.com>
2014-01-25 22:56:10 -0500

    It works, v2

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,20 +2,28 @@
 using System.Collections;
 
 public class UI : MonoBehaviour {
-//IGNORE THIS FOR NOW! I SWEAR I WILL USE A BEARABLE (hehe bear) SYSTEM SOON!
-	// Use this for initialization
-
+//Displays a grid of items to use
 	public static int selGridInt = 0;
 	public string[] selStrings = new string[] {"Empty", "Windbreaker", "Umbrella", "Sunglasses", "Diet Soda",  "Hard Hat", "Heels", "Spacesuit"};
 	private int gridXPos = Screen.width/2 - 350; 
 	private int gridYPos = Screen.height/8 -50; 
 	private int gridXSize = 800;
 	private int gridYSize = 30;
+	void Start(){
+		//anim = GetComponent<Animator>();
+	}
+
+	
 	void OnGUI () {
 		//Item equiped to recieved, checked for GUI changes, and is set again.
 		selGridInt = (int) Player.itemEquiped;
 		selGridInt = GUI.SelectionGrid(new Rect(gridXPos, gridYPos, gridXSize, gridYSize), selGridInt, selStrings, 09);
-        Player.itemEquiped = (Items) selGridInt;
+		if(selGridInt != (int)Player.itemEquiped){
+			Debug.Log("NEW!");
+			Player.itemEquiped = (Items) selGridInt;
+		}
+		Player.itemEquiped = (Items) selGridInt;
+
 		}
 }
  
