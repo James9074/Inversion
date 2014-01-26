848c368deec01fe58115c3f6a6fb4911c1ec6311
Eric King <emking92@gmail.com>
2014-01-25 23:40:48 -0500

    Added heels handler

---
+++ Assets/Scripts/ShoeHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoeHandler : MonoBehaviour 
+{
+
+    bool PrevIsShoes;
+    	
+	// Update is called once per frame
+	void Update () 
+    {
+        bool isShoes = Player.itemEquiped == Items.Heels;
+
+        if (isShoes != PrevIsShoes)
+        {
+            float scale = (isShoes) ? .5f : 1f;
+
+            Vector3 localScale = transform.localScale;
+
+            localScale.x = scale;
+            localScale.y = scale;
+
+            transform.localScale = localScale;
+
+            PrevIsShoes = isShoes;
+        }
+	}
+}
