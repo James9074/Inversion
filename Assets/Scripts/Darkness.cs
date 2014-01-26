9734c3e86db8f99b494cf62111a40a877cf9e3c6
Eric King <emking92@gmail.com>
2014-01-26 00:19:24 -0500

    Added darkness for the sunglasses

---
+++ Assets/Scripts/Darkness.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Darkness : MonoBehaviour 
+{
+
+    bool prevEnabled = true;
+    	
+	// Update is called once per frame
+	void Update () 
+    {
+        bool enabled = !(Player.itemEquiped == Items.Sunglasses);
+
+        if (enabled != prevEnabled)
+        {
+            renderer.enabled = enabled;
+            prevEnabled = enabled;
+        }
+	}
+}
