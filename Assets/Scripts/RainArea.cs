ff05fa119afdf1a1768e6ad2865a0f9c0093bd03
Eric King <emking92@gmail.com>
2014-01-26 11:45:30 -0500

    Added rain

---
+++ Assets/Scripts/RainArea.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainArea : MonoBehaviour
+{
+    public GameObject RainDrop;
+    public float SpawnRate;
+    public float DropSpeed;
+
+    BoxCollider2D area;
+    private float spawnY;
+    private float spawnLeft;
+    private float spawnRight;
+    private float height;
+
+    private float spawnTimer;
+
+    void Start()
+    {
+         area = GetComponent<BoxCollider2D>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Player.itemEquiped == Items.Umbrella)
+        {
+            UpdateArea();
+
+            spawnTimer += Time.deltaTime;
+
+            if (spawnTimer > SpawnRate)
+            {
+                int numSpawns = (int)(spawnTimer / SpawnRate);
+
+                for (int i = 0; i < numSpawns; i++)
+                {
+                    SpawnRainDrop();
+                }
+                spawnTimer = 0;
+            }
+        }
+	}
+
+    private void UpdateArea()
+    {
+        spawnY = transform.position.y + area.size.y / 2 + area.center.y;
+        spawnLeft = transform.position.x - area.size.x / 2;
+        spawnRight = transform.position.x + area.size.x / 2;
+        height = area.size.y;
+    }
+
+    private void SpawnRainDrop()
+    {
+        Vector2 spawn = RandomSpawnPoint();
+
+        GameObject dropObject = GameObject.Instantiate(RainDrop, spawn, Quaternion.identity) as GameObject;
+        RainDrop drop = dropObject.GetComponent<RainDrop>();
+        drop.MaxDistance = height;
+        drop.Speed = DropSpeed;
+    }
+
+    private Vector2 RandomSpawnPoint()
+    {
+        float spawnX = Random.Range(spawnLeft, spawnRight);
+        return new Vector2(spawnX, spawnY);
+    }
+}
---
+++ Assets/Scripts/RainDrop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainDrop : MonoBehaviour 
+{
+    public float Speed;
+    public float MaxDistance
+    {
+        set
+        {
+            MaxDistanceSqr = value * value;
+        }
+    }
+    
+    private float MaxDistanceSqr;
+
+    private Vector3 origin;
+
+	// Use this for initialization
+	void Start () 
+    {
+        origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 position = transform.position;
+        position -= Vector3.up * Speed * Time.deltaTime;
+        transform.position = position;
+
+        float distanceSqr = (position - origin).sqrMagnitude;
+
+        if (distanceSqr > MaxDistanceSqr)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+	}
+}
