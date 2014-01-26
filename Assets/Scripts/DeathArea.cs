4e00329cb1f2840a0b2b315b30728c043ae17557
Eric King <emking92@gmail.com>
2014-01-26 15:50:30 -0500

    Added fire animation Added spikes

---
+++ Assets/Scripts/DeathArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathArea : MonoBehaviour 
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            collider.gameObject.GetComponent<Player>().Kill();
+        }
+    }
+}
