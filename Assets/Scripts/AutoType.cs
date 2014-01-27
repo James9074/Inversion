166bdfd78e586407a0ab62fa1a14d9bd791d1185
James Thomas <james9074@gmail.com>
2014-01-27 14:14:07 -0500

    Added Dialogue Boxes, prettying up the landscape, other small fixes.

---
+++ Assets/Scripts/AutoType.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoType : MonoBehaviour {
+	
+	public float letterPause = 0.2f;
+	public AudioClip sound;
+	public Items item;
+
+	string message;
+	
+	// Use this for initialization
+	void Start () {
+		//guiText.text = "You have recieved: ... " + item;
+		//message = guiText.text;
+		guiText.text = "";
+		StartCoroutine(TypeText ());
+
+	}
+
+	void TheStart(string s){
+		message = s;
+	}
+	
+	IEnumerator TypeText () {
+
+		foreach (char letter in message.ToCharArray()) {
+			guiText.text += letter;
+			if (sound)
+				audio.PlayOneShot(sound);
+			yield return 0;
+			yield return new WaitForSeconds (letterPause);
+
+		}
+		yield return new WaitForSeconds(2);
+		Destroy(this.gameObject);
+	}
+
+	public enum Items
+{
+	Empty,
+	Heels,
+	DietSoda,
+	Windbreaker,
+	HardHat,
+	Umbrella,
+	Sunglasses,
+	Spacesuit
+};
+
+
+}
--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -4,14 +4,31 @@ using System.Collections;
 public class ItemPickup : MonoBehaviour
 {
     public Items item;
-
+	public GameObject popupBox;
+	private string popupText;
     void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log(collider.gameObject.name);
         if (collider.gameObject.name == "Player")
         {
-            Player.UnlockItem(item);
+
+			GameObject poofObj = (GameObject)Instantiate(popupBox, new Vector2(.25f,.75f), Quaternion.identity);
+			popupText = "You have found the "+item+"!";
+			if (popupText == "You have found the DietSoda!")
+				popupText = "You have found the Diet Soda!";
+			if (popupText == "You have found the HartHat!")
+				popupText = "You have found the Hard Hat!";
+			poofObj.SendMessage("TheStart",popupText);
+			Player.UnlockItem(item);
             GameObject.Destroy(this.gameObject);
         }
+
+
+
     }
+
+
+
+
+
 }
